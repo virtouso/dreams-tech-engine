a5f203cb3a254800936cbee5936a669ab2d61c3a
LAPTOP-9KQD6BO4\s_moe <s.moeen.kamali@gmail.com>
2021-07-26 00:05:28 +0430

    fixed the bulshit nad have both imgui and sfml together

--- DreamsTech/DreamsTech/Main.cpp
+++ DreamsTech/DreamsTech/Main.cpp
@@ -1,27 +1,41 @@
-#include <SFML/Graphics.hpp>
+#include "imgui.h"
+#include "imgui-SFML.h"
 
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
 
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
 
- 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
         }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
 
         window.clear();
         window.draw(shape);
+        ImGui::SFML::Render(window);
         window.display();
     }
 
-    return 0;
+    ImGui::SFML::Shutdown();
 }
